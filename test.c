f44d1d82f5c8fb891a35849d43521c030c5bf346
Mary Jane <maryj@greenwich_eduroam89-8.hampshire.edu>
2024-08-08 09:20:25 -0400

    8x speed improvement eliminating the prefetches

--- test.c
+++ test.c
@@ -1,4 +1,4 @@
-#include "new.h"
+#include "ecs.h"
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
@@ -17,8 +17,8 @@ void component_sum_callback(ent_t ent, void* data, void* context) {
     }
 }
 
-double get_time_in_seconds(struct timespec start, struct timespec end) {
-    return (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+double get_time_in_nanoseconds(struct timespec start, struct timespec end) {
+    return (end.tv_sec - start.tv_sec) * 1e9 + (end.tv_nsec - start.tv_nsec);
 }
 
 int main() {
@@ -35,15 +35,15 @@ int main() {
         memset(&ecs, 0, sizeof(ecs));
 
         // Creating entities
-        clock_gettime(CLOCK_MONOTONIC, &start);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
         for (size_t i = 0; i < MAX_ENTS; ++i) {
             create(&ecs);
         }
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        total_create_duration += get_time_in_seconds(start, end);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+        total_create_duration += get_time_in_nanoseconds(start, end);
 
         // Adding components to entities
-        clock_gettime(CLOCK_MONOTONIC, &start);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
         int data[MAX_CMP_SIZE / sizeof(int)];
         for (size_t i = 0; i < MAX_CMP_SIZE / sizeof(int); ++i) {
             data[i] = i;
@@ -53,44 +53,44 @@ int main() {
                 add(&ecs, i, j, data, sizeof(data));
             }
         }
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        total_add_duration += get_time_in_seconds(start, end);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+        total_add_duration += get_time_in_nanoseconds(start, end);
 
         // Saving ECS state
-        clock_gettime(CLOCK_MONOTONIC, &start);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
         save(&ecs, SAVE_FILE);
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        total_save_duration += get_time_in_seconds(start, end);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+        total_save_duration += get_time_in_nanoseconds(start, end);
 
         // Loading ECS state
         struct ECS loaded_ecs = {0};
-        clock_gettime(CLOCK_MONOTONIC, &start);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
         load(&loaded_ecs, SAVE_FILE);
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        total_load_duration += get_time_in_seconds(start, end);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+        total_load_duration += get_time_in_nanoseconds(start, end);
 
         // Iterating over entities
         int sum = 0;
-        clock_gettime(CLOCK_MONOTONIC, &start);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
         iterate(&loaded_ecs, component_sum_callback, &sum);
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        total_iterate_duration += get_time_in_seconds(start, end);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+        total_iterate_duration += get_time_in_nanoseconds(start, end);
 
         // Destroying entities
-        clock_gettime(CLOCK_MONOTONIC, &start);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
         for (size_t i = 0; i < loaded_ecs.ent_count; ++i) {
             destroy(&loaded_ecs, loaded_ecs.ents[i]);
         }
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        total_destroy_duration += get_time_in_seconds(start, end);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+        total_destroy_duration += get_time_in_nanoseconds(start, end);
     }
 
-    printf("Average time taken to create %d entities: %f seconds\n", MAX_ENTS, total_create_duration / NUM_ITERATIONS);
-    printf("Average time taken to add %d components to each of %d entities: %f seconds\n", MAX_CMPS, MAX_ENTS, total_add_duration / NUM_ITERATIONS);
-    printf("Average time taken to save ECS state: %f seconds\n", total_save_duration / NUM_ITERATIONS);
-    printf("Average time taken to load ECS state: %f seconds\n", total_load_duration / NUM_ITERATIONS);
-    printf("Average time taken to iterate over %d entities and sum components: %f seconds\n", MAX_ENTS, total_iterate_duration / NUM_ITERATIONS);
-    printf("Average time taken to destroy %d entities: %f seconds\n", MAX_ENTS, total_destroy_duration / NUM_ITERATIONS);
+    printf("Average time taken to create %d entities: %.2f nanoseconds\n", MAX_ENTS, total_create_duration / NUM_ITERATIONS);
+    printf("Average time taken to add %d components to each of %d entities: %.2f nanoseconds\n", MAX_CMPS, MAX_ENTS, total_add_duration / NUM_ITERATIONS);
+    printf("Average time taken to save ECS state: %.2f nanoseconds\n", total_save_duration / NUM_ITERATIONS);
+    printf("Average time taken to load ECS state: %.2f nanoseconds\n", total_load_duration / NUM_ITERATIONS);
+    printf("Average time taken to iterate over %d entities and sum components: %.2f nanoseconds\n", MAX_ENTS, total_iterate_duration / NUM_ITERATIONS);
+    printf("Average time taken to destroy %d entities: %.2f nanoseconds\n", MAX_ENTS, total_destroy_duration / NUM_ITERATIONS);
 
     return 0;
 }
